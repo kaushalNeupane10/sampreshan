bf5cf9ae2818cad2930f77407d948d1ef18f8ad8
Kaushal Neupane <kaushal98211@gmail.com>
2026-07-10 14:08:34 +0545

    initial phase setup of hero section of studio page

--- frontend/src/app/studio/page.tsx
+++ frontend/src/app/studio/page.tsx
@@ -1,9 +1,11 @@
 import StudioCard from "@/components/studio/StudioCard";
+import StudioHero from "@/components/studio/StudioHero";
 import { studioData } from "@/data/studio";
 
 export default function page() {
   return (
     <>
+      <StudioHero />
       <section className="container-custom py-16 lg:py-24">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
           {studioData.map((studio) => (
